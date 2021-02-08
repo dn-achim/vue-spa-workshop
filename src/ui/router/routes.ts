8ecc43b9f883d474fb186d7c32bba7c6f2adf829
Achim Gosse <achim.gosse@digitalnoise.de>
2021-02-08 19:32:19 +0100

    Added home view. Removed old views.

--- src/ui/router/routes.ts
+++ src/ui/router/routes.ts
@@ -1,15 +1,9 @@
 import { RouteConfig } from 'vue-router'
-import Home from '@/ui/views/Home.vue'
 
 export const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "view-about" */ '../views/About.vue')
+    name: 'home',
+    component: () => import(/* webpackChunkName: "view-home" */ '../views/home/home.vue')
   }
 ]
---
+++ src/ui/views/home/home.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-row>
+    <v-col
+      cols="12"
+      sm="10"
+      md="8"
+      xl="6"
+      class="mx-auto"
+    >
+      <v-card
+        class="pa-4"
+      >
+        <v-img
+          :src="require('../../assets/logo.png')"
+          alt="digitalnoise GmbH"
+        />
+        <v-card-text
+          class="body-1 text-justify"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </v-card-text>
+        <v-card-text
+          class="body-1 text-justify"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  metaInfo: {
+    title: 'Startseite'
+  }
+})
+export default class HomeView extends Vue {
+}
+</script>
